d4ea0fc830b067bc4ca5dc8a690c867bf00a1dfd
Matt Horn <mathphreak@gmail.com>
2015-12-19 11:51:47 -0700

    Report how much money the player has

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ extern crate rustc_serialize;
 
 use nickel::{Nickel, HttpRouter};
 use std::io::Read;
+use std::collections::HashMap;
 use rustc_serialize::json;
+use nickel::StaticFilesHandler;
+use std::sync::{Arc, Mutex};
 
-#[derive(RustcEncodable, RustcDecodable)]
+#[derive(RustcEncodable, RustcDecodable, Copy, Clone)]
 struct State {
     armor: u32,
     burning: u32,
@@ -18,24 +21,36 @@ struct State {
     smoked: u32
 }
 
-#[derive(RustcEncodable, RustcDecodable)]
+#[derive(RustcEncodable, RustcDecodable, Copy, Clone)]
 struct Player {
     state: State
 }
 
-#[derive(RustcEncodable, RustcDecodable)]
+#[derive(RustcEncodable, RustcDecodable, Copy, Clone)]
 struct Message {
     player: Player
 }
 
 fn main() {
     let mut server = Nickel::new();
+    let current_player_state = Arc::new(Mutex::new(State{
+        armor: 0,
+        burning: 0,
+        flashed: 0,
+        health: 0,
+        helmet: false,
+        money: 0,
+        round_killhs: 0,
+        round_kills: 0,
+        smoked: 0
+    }));
+    let current_player_state2 = current_player_state.clone();
 
     server.post("/", middleware! { |request, response|
         let mut body = String::new();
         request.origin.read_to_string(&mut body).unwrap();
         let data: Message = match json::decode(&body) {
-            Ok(num) => num,
+            Ok(data) => data,
             Err(_) => {
                 println!("got bad JSON: {}", body);
                 Message{
@@ -55,9 +70,20 @@ fn main() {
                 }
             },
         };
+        let mut current_player_state = current_player_state.lock().unwrap();
+        *current_player_state = data.player.state;
         println!("You have ${}", data.player.state.money);
         "Thanks"
     });
+
+    server.get("/", middleware! { |_, response|
+        let mut data = HashMap::new();
+        let current_player_state = current_player_state2.lock().unwrap();
+        data.insert("money", (*current_player_state).money);
+        return response.render("assets/index.tpl", &data)
+    });
+
+    server.utilize(StaticFilesHandler::new("assets/vendor/"));
 
     server.utilize(router! {
         get "**" => |_req, _res| {
