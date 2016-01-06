291116fad36281d41244c43f5e6f00eeb376f38d
Matt Horn <mathphreak@gmail.com>
2016-01-06 15:29:48 -0700

    Launch UI when server starts

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,34 @@ use std::sync::{Arc, Mutex};
 use rustc_serialize::json::ToJson;
 use cs_eco_dash::{game, gsi};
 use cs_eco_dash::gsi::message::{TakesUpdates, UpdateReason};
+use std::process::Command;
+
+// lifted from https://github.com/euclio/aurelius/blob/master/src/browser.rs,
+fn launch_ui() {
+    let (browser, args) = if cfg!(target_os = "linux") {
+        ("xdg-open", vec![])
+    } else if cfg!(target_os = "macos") {
+        ("open", vec!["-g"])
+    } else if cfg!(target_os = "windows") {
+        // originally this was ("start", vec![""])
+        // but that caused weird issues, since start is a cmd builtin
+        
+        // `start` requires an empty string as its first parameter.
+        ("cmd", vec!["/c", "start", ""])
+    } else {
+        println!("What even is your OS");
+        return;
+    };
+
+    let process = Command::new(browser)
+        .args(&args)
+        .arg("http://localhost:3000".to_string())
+        .status();
+
+    if let Err(e) = process {
+        println!("Couldn't launch browser {}: {}", browser, e);
+    }
+}
 
 fn main() {
     let mut server = Nickel::new();
@@ -24,6 +52,8 @@ fn main() {
     });
 
     server.utilize(StaticFilesHandler::new("assets/"));
+
+    launch_ui();
 
     server.listen("127.0.0.1:3000");
 }
